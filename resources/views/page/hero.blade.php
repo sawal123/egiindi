70582ada84f0036848b480816a37e51d7c139c56
sawal123 <sawalinto@gmail.com>
2024-04-28 10:16:07 +0700

    feat: hero

--- resources/views/page/hero.blade.php
+++ resources/views/page/hero.blade.php
@@ -3,8 +3,8 @@
         <div class="row align-items-center"> <!-- Tambahkan kelas align-items-center untuk mengatur vertikal tengah -->
             <div class="col-lg-6 col-12 ">
                 {{-- <div class="d-flex flex-column justify-content-center h-100"> <!-- Tambahkan kelas flex-column dan justify-content-center untuk mengatur vertikal tengah --> --}}
-                <h1 class="fw-bold text-light">Hi, Saya Egi</h1>
-                <h2 class="text-light">Salesman Indihome</h2>
+                <h1 class="fw-bold text-light">Solusi Internet Cepat, Berkelas, dan Cerdas untuk Aktivitas Tanpa Batas</h1>
+                {{-- <h2 class="text-light">Salesman Indihome</h2> --}}
                 <p class="fw-medium text-light">
                     Dengan IndiHome, kini mendaftar layanan internet tidak perlu lagi merepotkan! Nikmati kemudahan mendaftar langsung dari rumah, tanpa harus keluar dan meninggalkan kesibukan Anda. Layanan kami tersedia di sejumlah wilayah, termasuk Medan dan sekitarnya seperti Tembung, Marelan, Johor, Helvetia, Padang Bulan, Amplas, bahkan hingga Tanjung Morawa. Jadi, tunggu apa lagi? Segera dapatkan koneksi internet berkualitas dari kenyamanan rumah Anda sendiri!
                 </p>
